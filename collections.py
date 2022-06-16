c0c76d4a43825e4da3c4ab4da48021a4b434aa21
Greta <97620006+GretaPet@users.noreply.github.com>
2022-06-16 09:27:12 +0100

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,31 @@
+myFruitList = ["apple", "banana", "cherry"] #Defining a list
+print(myFruitList) 
+print(type(myFruitList)) 
+
+print(myFruitList[0]) #Accessing a list by position
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange" #Changing the values in a list
+print(myFruitList)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple") #Defining a tuple
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0]) #Accessing a tuple by position
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+myFavoriteFruitDictionary = { #Defining a dictionary
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"]) #Accessing a dictionary by name
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
---
+++ numeric-data.py
@@ -0,0 +1,26 @@
+print("Python has three numeric types: int, float, and complex")
+
+myValue=1
+print(myValue) #function to write the value of the variable to the shell
+print(type(myValue)) #built in function to get the data type of the variable
+print(str(myValue) + " is of the data type " + str(type(myValue))) #converts an argument into a collection of letters called a string
+print()
+myValue2=3.14
+print(myValue2) #write the value of the variable to the shell
+print(type(myValue2)) #Get the data type of the variable
+print(str(myValue2) + " is of the data type " + str(type(myValue2))) #combine numbers and text
+print()
+myValue3=5j
+print(myValue3)
+print(type(myValue3))
+print(str(myValue3) + " is of the data type " + str(type(myValue3)))
+print()
+myValue4=True
+print(myValue4)
+print(type(myValue4))
+print(str(myValue4) + " is of the data type " + str(type(myValue4)))
+print()
+myValue5=False
+print(myValue5)
+print(type(myValue5))
+print(str(myValue5) + " is of the data type " + str(type(myValue5)))
